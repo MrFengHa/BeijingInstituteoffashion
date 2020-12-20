0e8f5655d3352c8f8ffcec709f8ba845d9630227
MrFengHa <1109179567@qq.com>
2020-12-21 02:50:26 +0800

    从新上传微信前端

--- el-qrcode/src/components/Main.vue
+++ el-qrcode/src/components/Main.vue
@@ -30,21 +30,16 @@
     data() {
       return {
         id: "",
-        url: 'https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg',
-        srcList: [
-          'https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg',
-          'https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg'
-        ],
-        audioURL: "http://39.102.41.207/audio/%E8%97%8F%E6%97%8F%E5%85%AB%E7%91%9E%E5%AE%9Dcn.mp3",
-        imgURL:"http://39.102.41.207/image/藏族八瑞宝.jpg"
+        audioURL: "http://39.102.41.207/001/cn.mp3",
+        imgURL:"http://39.102.41.207/001/image.jpg"
 
       }
     }, methods: {
       changeLanguage(sign) {
         if (sign > 0) {
-          this.audioURL = "http://39.102.41.207/audio/%E8%97%8F%E6%97%8F%E5%85%AB%E7%91%9E%E5%AE%9Dcn.mp3"
+          this.audioURL = "http://39.102.41.207/001/cn.mp3"
         } else {
-          this.audioURL = "http://39.102.41.207/audio/藏族八瑞宝en.mp3"
+          this.audioURL = "http://39.102.41.207/001/en.mp3"
         }
       },
 
