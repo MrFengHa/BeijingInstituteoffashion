b3a3fac5d1cb224dc8db0dd309dc69b2a9d63aff
MrFengHa <1109179567@qq.com>
2020-12-22 16:06:56 +0800

    managerService

---
+++ runmanager/src/main/java/com/heiyou/RunmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.heiyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunmanagerApplication.class, args);
+    }
+
+}
